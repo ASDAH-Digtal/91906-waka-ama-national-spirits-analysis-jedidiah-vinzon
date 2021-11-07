1ca64bda2c972529fa015b86e37dc92951b572ef
jedidiah-vinzon <jedidiah_vin@student.asdah,school.nz>
2021-11-08 09:20:33 +1300

    modified read_files function; neater and working

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
 # Imports
 from tkinter import *
 from tkinter import ttk
+import os
 
 # Where functions are made
 def read_files():
-    f = open("waka\\WakaNats{}".format(year.get()), "r")
-    pass
+    files = []
+    keyword = "final"
 
+    path = "waka\\WakaNats{}".format(year.get())
+    dirs = os.listdir(path)
+
+    for file in dirs:
+        if keyword in file.lower():
+            files.append(file)
+        else: pass
+    
+    number_of_files = len(files)
+    results_text.set("There are {} files about the finals in the year {}".format(number_of_files, year.get()))
+
+def analyse_files():
+    pass
 
 # Setting up root
 root = Tk()
 root.title("Waka Ama New Zealand Club")
-root.geometry("500x500")
+
+# title_label = ttk.Label(root, text="Waka Ama New Zealand Club").grid(row=0, column=0)
 
 # Variables
 year = StringVar()
-year.set("2017")
+results_text = StringVar()
 
 # Setting up frames
-frame1 = ttk.Frame(root)
-frame1.grid(row=0, column=0, sticky="NSEW")
+frame1 = ttk.LabelFrame(root, text="Available Folders")
+frame1.grid(row=0, column=0, padx=20, pady=20)
 
+frame2 = ttk.LabelFrame(root, text="Results")
+frame2.grid(row=0, column=1, padx=20, pady=20)
 
 # What's inside frame1?
-# title_label = ttk.Label(frame1, text="Waka Ama New Zealand Club", justify="center", anchor="center").grid(row=1, column=0, sticky="EW")
-analyse_label = ttk.Label(frame1, text="Folders available for analysing: ").grid(row=2, column=0)
+label1 = ttk.Label(frame1, text="These are the available files to analyse", justify="center").grid(row=1, columnspan=2)
+option_2017 = ttk.Radiobutton(frame1, text="2017", variable=year, value="2017").grid(row=2, column=0)
+option_2018 = ttk.Radiobutton(frame1, text="2018", variable=year, value="2018").grid(row=3, column=0)
 
-button17 = ttk.Radiobutton(frame1, variable=year, value="2017", text="2017").grid(row=2, column=1)
-button18 = ttk.Radiobutton(frame1, variable=year, value="2018", text="2018").grid(row=2, column=2)
-analyse_button = ttk.Button(frame1, command=read_files).grid(row=3, column=1)
+check_button = ttk.Button(frame1, text="Check files", command=read_files).grid(row=4, column=0, columnspan=3)
+analyse_button = ttk.Button(frame1, text="Analyse", command=analyse_files).grid(row=5, column=0, columnspan=3)
+
+# What's inside frame2?
+results = ttk.Label(frame2, textvariable=results_text, justify="center")
+results_text.set("The results of your query will be found here!")
+results.grid(row=0, column=0)
 
 # Run everything
 root.mainloop()
