87dafe8512c8f2a57558d2376d58924102b51c1b
jedidiah-vinzon <jedidiah_vin@student.asdah,school.nz>
2021-11-15 10:11:03 +1300

    tinkered and modified code here and there

--- main.py
+++ main.py
@@ -5,16 +5,36 @@ import time
 import os
 
 # Where classes are made
-class Player:
-    def __init__(self, place, id, team_name, regional_association):
-        self.place = place
-        self.id = id
-        self.team_name = team_name
-        self.regional_association = regional_association
+class Regional:
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+    
+    def add_points(self, position):
+        if position == 1:
+            self.points += 8
+        elif position == 2:
+            self.points += 7
+        elif position == 3:
+            self.points += 6
+        elif position == 4:
+            self.points += 5
+        elif position == 5:
+            self.points += 4
+        elif position == 6:
+            self.points += 3
+        elif position == 7:
+            self.points += 2
+        else:
+            self.points += 1
+
+    def print_obj(self):
+        print(self.name, self.points)
 
 
 # Where functions are made
 def read_files():
+    """Finds the files to be read with the keyword: 'final'; and prints how many there are"""
     files = []
     keyword = "final"
 
@@ -31,25 +51,35 @@ def read_files():
     results_text.set("There are {} files about the finals in the year {}".format(number_of_files, year.get()))
 
 def analyse_files():
+    """Analyses all the files with the keyword: 'final'; and shows all the points for each Regional Association"""
     files = []
+    reg_ass = []
     keyword = "final"
+    count = 0
 
     path = "waka\\WakaNats{}".format(year.get())
     dirs = os.listdir(path)
 
-    for file in dirs:
-        
-        # file_read.configure(text="Reading: {}".format(str(file)))
-        if keyword in file.lower():
-            files.append(file)
-        else: pass
-    
-    # number_of_files = len(files)
-    # results_text.set("There are {} files about the finals in the year {}".format(number_of_files, year.get()))
+    for file in dirs:               # runs through all the files in the directory
+        if keyword in file.lower(): # checks if all the files found have the keyword in it
+            files.append(file)      # adds the files to the list called 'files'
+        else: pass                  # otherwise, the loop passes the file
 
     for game in files:
         f = open(game, "r")
-        pass
+
+        for line in f.readlines():
+            if count == 0:
+                pass
+            else:
+                if line[5] in reg_ass:
+                    pass
+                else:
+                    temp_obj = Regional(line[5], 0)
+                    temp_obj.add_points(int[line[0]])
+                    reg_ass.append(temp_obj)
+            
+            count += 1
 
 # Setting up root
 root = Tk()
@@ -61,6 +91,7 @@ root.title("Waka Ama New Zealand Club")
 year = StringVar()
 results_text = StringVar()
 file_read_text = StringVar()
+error_text = StringVar()
 
 # Setting up frames
 frame1 = ttk.LabelFrame(root, text="Available Folders")
